a906ad7f89477e742e764fed1e65f5f61a4bb2f3
Yassine Messaoudi <messaoudiyassine4@gmail.com>
2020-10-10 14:28:14 +0100

    feat: Introduce test for login and logout.

--- pwmtest/db_test.go
+++ pwmtest/db_test.go
@@ -8,21 +8,31 @@ import (
 )
 
 func TestCreateTables(t *testing.T) {
+	if err := psql.IstablishAndCreateDB(); err != nil {
+		t.Log("Cannot istablish cnx with postgres", err.Error())
+		t.Fail()
+		t.Log(err)
+	}
 	if err := os.Setenv("test", "true"); err != nil {
 		t.Fail()
 		t.Log(err)
 	}
 	db, err := psql.NewDb()
 	if err != nil {
+		t.Log("Failed to initialize db Connection")
 		t.Fail()
 		t.Log(err)
 	}
+	t.Log("Dropping Tables")
 	if err := db.DropTestTables(); err != nil {
+		t.Log("Cannot Drop tables")
 		t.Fail()
 		t.Log(err)
 	}
+	t.Log("Creating Tables")
 	err = db.CreateTables()
 	if err != nil {
+		t.Log("Cannot Create Tables")
 		t.Fail()
 		t.Log(err)
 	}
--- pwmtest/loginRegister_test.go
+++ pwmtest/loginRegister_test.go
@@ -27,8 +27,14 @@ type resp struct {
 
 func TestCreateUser(t *testing.T) {
 	cases := map[string]resp{
-		`{"username":"yassine","password":"test","email":"rand@gmail.com"}`: *(&resp{`{IsReg:false}`, 201}),
-		`{"username":"mark","password":"test","email":"rand@gmail.com"}`:    *(&resp{`{IsReg:true}`, 409}),
+		`{"username":"yassine","password":"test","email":"rand@gmail.com"}`: resp{
+			resJSON:    `{IsReg:false}`,
+			statusCode: 201,
+		},
+		`{"username":"mark","password":"test","email":"rand@gmail.com"}`: resp{
+			resJSON:    `{IsReg:true}`,
+			statusCode: 409,
+		},
 	}
 	for k, v := range cases {
 		data := []byte(k)
@@ -37,6 +43,8 @@ func TestCreateUser(t *testing.T) {
 			t.Log(err)
 		}
 		if rr.Code != v.statusCode && rr.Body.String() != v.resJSON {
+			t.Log(v.statusCode, v.resJSON)
+
 			t.Log(rr.Code, rr.Body.String())
 			t.Fail()
 		}
@@ -48,7 +56,6 @@ func TestStartUser(t *testing.T) {
 		`{"username":"yassine","password":"test"}`:   *(&resp{`{IsLog:true}`, 200}),
 		`{"username":"yassine","password":"secure"}`: *(&resp{`{IsLog:false}`, 400}),
 	}
-
 	for k, v := range cases {
 		data := []byte(k)
 		rr, err := makeRequest("POST", "/login", data)
@@ -59,5 +66,58 @@ func TestStartUser(t *testing.T) {
 			t.Log(rr.Code, rr.Body.String())
 			t.Fail()
 		}
+	}
+}
+
+func TestLoginNewSession(t *testing.T) {
+	specialCase := struct {
+		data     string
+		response resp
+	}{
+		`{"username":"yassine","password":"test"}`, resp{
+			resJSON:    `{IsLog:false}`,
+			statusCode: 409,
+		},
+	}
+
+	rr, err := makeRequest("POST", "/login", []byte(specialCase.data))
+	if err != nil {
+		t.Log("Cannot make request")
+		t.Log(err)
+		t.Fail()
+	}
+	if rr.Code != specialCase.response.statusCode && rr.Body.String() != specialCase.response.resJSON {
+		t.Log(specialCase.response.statusCode, specialCase.response.resJSON)
+		t.Fail()
+	}
+
+}
+
+type logoutResponse struct {
+	statusCode int
+	jsonData   string
+}
+
+type logoutStuff struct {
+	requestData string
+	resp        logoutResponse
+}
+
+func TestLogout(t *testing.T) {
+	data := logoutStuff{
+		requestData: `{"username":"yassine"}`,
+		resp: logoutResponse{
+			statusCode: 200,
+			jsonData:   `IsLogout:true`,
+		},
+	}
+	rr, err := makeRequest("POST", "/logout", []byte(data.requestData))
+	if err != nil {
+		t.Fail()
+		t.Log(err)
+	}
+	if rr.Code != data.resp.statusCode && rr.Body.String() != data.resp.jsonData {
+		t.Log(rr.Code, rr.Body.String())
+		t.Fail()
 	}
 }
